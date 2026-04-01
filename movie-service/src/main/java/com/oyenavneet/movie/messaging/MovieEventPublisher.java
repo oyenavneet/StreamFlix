70fb8af94c3f7f9733d96dd8aff82d41b8c46c73
Navneet Srivastava <oyenavneet@gmail.com>
2026-04-02 02:17:31 +0530

    movie service - impl

--- movie-service/src/main/java/com/oyenavneet/movie/messaging/MovieEventPublisher.java
+++ movie-service/src/main/java/com/oyenavneet/movie/messaging/MovieEventPublisher.java
@@ -1,13 +1,32 @@
 package com.oyenavneet.movie.messaging;
 
 import com.oyenavneet.streamflix.events.MovieAddedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.event.EventListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MovieEventPublisher {
+    private static final String MOVIE_EVENTS_OUT = "movie-events-out";
+    private static final Logger logger = LoggerFactory.getLogger(MovieEventPublisher.class);
+    private final StreamBridge streamBridge;
+
+    public MovieEventPublisher(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
+
 
     @EventListener
     public void onMovieAdded(MovieAddedEvent movieAddedEvent) {
+        var message = MessageBuilder.withPayload(movieAddedEvent)
+                .setHeader(KafkaHeaders.KEY, movieAddedEvent.movieId())
+                .build();
+        streamBridge.send(MOVIE_EVENTS_OUT, message);
+        logger.info("Movie published: {}", message);
+
     }
 }
--- movie-service/src/main/java/com/oyenavneet/movie/service/MovieService.java
+++ movie-service/src/main/java/com/oyenavneet/movie/service/MovieService.java
@@ -1,17 +1,36 @@
 package com.oyenavneet.movie.service;
 
 import com.oyenavneet.movie.dto.MovieDetails;
+import com.oyenavneet.movie.exception.MovieNotFoundException;
+import com.oyenavneet.movie.mapper.MovieMapper;
+import com.oyenavneet.movie.repository.MovieRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
 
+    private final MovieRepository movieRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    public MovieService(MovieRepository movieRepository, ApplicationEventPublisher eventPublisher) {
+        this.movieRepository = movieRepository;
+        this.eventPublisher = eventPublisher;
+    }
+
     public MovieDetails getMovie(Integer movieId) {
-        return null;
+        return this.movieRepository.findById(movieId)
+                .map(MovieMapper::toMovieDetails)
+                .orElseThrow(() -> new MovieNotFoundException(movieId));
     }
 
 
+    @Transactional
     public MovieDetails saveMovie(MovieDetails movieDetails) {
-        return null;
+
+        var movie = this.movieRepository.save(MovieMapper.toMovie(movieDetails));
+        this.eventPublisher.publishEvent(MovieMapper.toMovieAddedEvent(movie));
+        return MovieMapper.toMovieDetails(movie);
     }
 }
